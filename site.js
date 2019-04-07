3a97ca31370ca2661bd404d2a008fb415903646b
aofayemi <42700790+aofayemi@users.noreply.github.com>
2019-04-07 15:32:51 -0500

    Adjust indentation to fix linting errors

--- site.js
+++ site.js
@@ -11,34 +11,35 @@
   // Unlike the raw operators these encapsulate, functions
   // can be passed around like any other value into other
   // functions.
-  function eq(value,condition) {
+  function eq(value, condition) {
     return value === condition;
   }
-  function gt(value,condition) {
+  function gt(value, condition) {
     return value > condition;
   }
-  function gte(value,condition) {
+  function gte(value, condition) {
     return value >= condition;
   }
-  function lt(value,condition) {
+  function lt(value, condition) {
     return value < condition;
   }
-  function lte(value,condition) {
+  function lte(value, condition) {
     return value <= condition;
   }
 
+  // Data cleanup functions
+  function clean_nonnumbers(value) {
+    // returns value with all non-digits removed
+    return value.replace(/\D/g, '');
+  }
+  function clean_whitespace(value) {
+    // returns value with all whitespace characters removed
+    return value.replace(/\s/g, '');
+  }
 
-    // Data cleanup functions
-    function clean_nonnumbers(value) {
-      // returns value with all non-digits removed
-      return value.replace(/\D/g,'');
-    }
-    function clean_whitespace(value) {
-      // returns value with all whitespace characters removed
-      return value.replace(/\s/g, '');
-    }
+  // Phone-specific santizier functions
+  function strip_us_country_code(value) {
+    return value.replace(/^1/, '');
+  }
 
-    // Phone-specific santizier functions
-    function strip_us_country_code(value) {
-      return value.replace(/^1/,'');
-    }
+}()); // End of IIFE
