77c29655b585371962e0f6d203db3a5f43ac0342
aofayemi <42700790+aofayemi@users.noreply.github.com>
2019-04-21 21:27:50 -0500

    Add local storage functions

--- site.js
+++ site.js
@@ -5,6 +5,34 @@
     // Old browser
     console.log('Old browser');
     return;
+  }// Function to be called later for determining localStorage support
+  // Taken from discussion at https://gist.github.com/paulirish/5558557
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+      x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
+
+  // Function to be called later for determining localStorage support
+  // Taken from discussion at https://gist.github.com/paulirish/5558557
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+      x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
   }
 
   // Data comparison function
@@ -73,6 +101,89 @@
   function validate_cvv(cvv) {
     // Valid security code is a 3 or 4 digit string with all whitespace removed
     return validate(remove_all_whitespace(cvv), /^[0-9]{3}[0-9]?$/g);
+  }
+
+  // Debounce function to limit calls on repeated events
+  // See for e.g., https://codeburst.io/throttling-and-debouncing-in-javascript-b01cad5c8edf
+  var debounce = function debounce(func, delay) {
+    var inDebounce;
+    return function () {
+      var context = this;
+      var args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(function () {
+        return func.apply(context, args);
+      }, delay);
+    };
+  };
+
+  // Functions for working with form data
+  // Return all data-rich input elements
+  function collectFormInputElements(form_selector) {
+    var saved_input_elements = [];
+    var input_elements = document.querySelector(form_selector).elements;
+    for (var input of input_elements) {
+      if(input.type !== 'submit') {
+        saved_input_elements.push(input);
+      }
+    }
+    return saved_input_elements;
+  }
+
+  // Get all interesting information about an input element
+ function getInputData(input_element) {
+   return {
+     id: input_element.id,
+     name: input_element.name,
+     type: input_element.tagName.toLowerCase(),
+     value: input_element.value
+   }
+ }
+
+ // Store a prefixed storage item with input item data
+  function storePrefixedInputStorageItem(prefix,input_element) {
+    var item_data = getInputData(input_element);
+    localStorage.setItem(prefix + item_data.id, JSON.stringify(item_data));
+  }
+
+  // Retrieve and parse an input storage item
+  function retrieveAndParseInputStorageItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  // Retrieve all prefixed storage item keys
+  function retrievePrefixedStorageItemKeys(prefix) {
+    var saved_keys = [];
+    for(var i = 0; i < localStorage.length; i++) {
+      // Get only the items that begin with `prefix`
+      var key = localStorage.key(i);
+      if (key.startsWith(prefix)) {
+        saved_keys.push(key);
+      }
+    }
+    return saved_keys;
+  }
+
+  function restorePrefixedFormInputsFromLocalStorage(prefix) {
+    // Get an array of all the prefixed stored keys
+    var saved_keys = retrievePrefixedStorageItemKeys(prefix);
+    // Loop through the array and use the stored object data to
+    // restore the value of the corresponding form item
+    for (var key of saved_keys) {
+      var item = retrieveAndParseInputStorageItem(key);
+      // Use old-school getElementById; no need to prefix with #
+      var input_by_id = document.getElementById(item.id)
+      if (input_by_id) {
+        input_by_id.value = item.value;
+      }
+    }
+  }
+
+  function destroyPrefixedStorageItemKeys(prefix) {
+    var keys_to_destroy = retrievePrefixedStorageItemKeys(prefix);
+    for (var key of keys_to_destroy) {
+      localStorage.removeItem(key);
+    }
   }
 
   // select the necessary elements from the DOM
@@ -186,7 +297,7 @@
         }
       });// end of IIFE event listener
     }// end of if statement checking the fetch api
-
+    
 
 
     // End of DOMContentLoaded
