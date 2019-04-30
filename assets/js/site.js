e082bb9d701372123205e81ffdaa70ffe8997272
Bryan Cruz <bcruzcastillo@hawk.iit.edu>
2019-04-30 16:59:34 -0500

    Make a single javascript file from the fragmented files using an if statement

--- assets/js/site.js
+++ assets/js/site.js
@@ -109,97 +109,227 @@
     };
   };
 
+  function confirmationTicket(num,size) {
+  // var number=Math.random()*10;
+  var number=Math.floor(Math.random()*100000);
+  var length=8;
+  var str = '' + number;
+  while (str.length < length) {
+  str = '0' + str;
+  }
+  return str;
+  }
+
+  function time(){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime;
+  }
+
   // ---------------------------select the necessary elements from the DOM--------------------------------
-  document.addEventListener('DOMContentLoaded', function(){
-    var signup_form = document.querySelector('#payment');
-    var signup_submit=document.querySelector('#submit');// submit button
+  var page1 = document.querySelector('#home');
+  var page2 = document.querySelector('#seating');
+  var page3 = document.querySelector('#check-out');
+  var page4 = document.querySelector('#confirmation');
 
-    // listen for keyup event anywhere in the form
-    signup_form.addEventListener('keyup', function(){
-      var email_input=document.querySelector('#email').value;
-      var phone_input=document.querySelector('#phonenumber').value;
-      var ccn_input=document.querySelector('#ccn').value;
-      var cvv_input=document.querySelector('#cvv').value;
-      var fname_input = document.querySelector('#fname').value;
-      var lname_input = document.querySelector('#lname').value;
+  if (page1 !== null && page1.id == 'home') {
+    console.log("Home page was found");
+    document.addEventListener('DOMContentLoaded', function(){
+      var event1 = document.querySelector('#event1')
+      var event2 = document.querySelector('#event2')
+      var event3 = document.querySelector('#event3')
+      var event4 = document.querySelector('#event4')
+      var event5 = document.querySelector('#event5')
+      var event6 = document.querySelector('#event6')
 
-      // create an object called checkout that will have all of the form's
-      // input placed into attributes for Local storage to use
-      var checkout = {
-        // TODO: implement get syntax to avoid repeating document.querySelector so dang much
-        email: document.querySelector('#email').value,
-        phone: document.querySelector('#phonenumber').value,
-        ccn: document.querySelector('#ccn').value,
-        cvv: document.querySelector('#cvv').value,
-        fname: fname_input,
-        lname: document.querySelector('#lname').value
-      };
+      var checkout = {};
 
-      // signup_submit.removeAttribute('disabled');
-      signup_submit.setAttribute('disabled', 'disabled');
-
-      // var contact_error = document.querySelector('#contact-error');
-      // Disable signup button if either email or phone number is filled.
-      if (validate_us_phone(phone_input) && validate_email(email_input) && validate_ccn(ccn_input) && validate_cvv(cvv_input) && fname_input!==null && lname_input!==null) {
-        signup_submit.removeAttribute('disabled');
-      }
-      else {
-        // show user error message
-        if(phone_input.length > 10) {
-          console.log("Invalid phone number!");
-        }
-        // This will re-disable the submmit button if the input changes to an invalid state
-        signup_submit.setAttribute('disabled', 'disabled');
-      }
-
-      if(storageAvailable('localStorage')){
-        signup_form.addEventListener('submit', function(){
+      event1.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price1').innerText;
+          console.log("checkout.price is: " + checkout.price);
           localStorage.setItem("payment_info", JSON.stringify(checkout));
-          console.log("Form submitted and data stored");
-        }); // end blog.form event listener
+        } // end if statement to check for localStorage
+      });
+      event2.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price2').innerText;
+          console.log("checkout.price is: " + checkout.price);
+          localStorage.setItem("payment_info", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+      event3.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price3').innerText;
+          console.log("checkout.price is: " + checkout.price);
+          localStorage.setItem("payment_info", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+      event4.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price4').innerText;
+          console.log("checkout.price is: " + checkout.price);
+          localStorage.setItem("payment_info", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+      event5.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price5').innerText;
+          console.log("checkout.price is: " + checkout.price);
+          localStorage.setItem("payment_info", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+      event6.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          checkout.price = document.querySelector('#ticket-price6').innerText;
+          console.log("checkout.price is: " + checkout.price);
+          localStorage.setItem("payment_info", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+    });  // End of DOMContentLoaded
+  }
+  else if (page2 !== null && page2.id == 'seating') {
+    console.log("Seating page was found");
+    document.addEventListener('DOMContentLoaded', function(){
+      if(storageAvailable('localStorage')){
+        var checkout = JSON.parse(localStorage.getItem('payment_info'));
+        localStorage.removeItem('payment_info')
       } // end if statement to check for localStorage
 
-    });// end of key up listener
+      console.log(checkout.price);
+      document.getElementById("ticket-price").innerHTML = checkout.price;
 
-    var location = {
-      zip: document.querySelector('#zip'),
-      state: document.querySelector('#state'),
-      city: document.querySelector('#city')
-    };
+      var section = document.querySelector('#sectionlevel')
+      section.addEventListener("change", function(){
+        var ticketTotal = Number(section.value) * Number(clean_nonnumber(checkout.price));
+        checkout.ticketTotal = ticketTotal;
+        document.getElementById("ticket-price").innerHTML = "$" + ticketTotal;
+      });
 
-    // Function that calls zippopotam to fill out the form given the zipcode
-    if('fetch' in window) {
-      console.log("yay, this browser suppports the Fetch API");
-      location.city.classList.add('fade-out');
-      location.city.classList.add('fade-out');
+      var quantity = document.querySelector('#seat-quantity')
+      quantity.addEventListener("change", function(){
+        checkout.ticketNum = Number(quantity.value)
+        checkout.total = checkout.ticketNum * checkout.ticketTotal;
+        document.getElementById("total").innerHTML = "$" + checkout.total;
+      });
 
-      var zip;
-      location.zip.addEventListener('keyup', function() {
-        if (validate_zip(location.zip.value) && zip !== location.zip.value){
-          zip = location.zip.value;
-          fetch('http://api.zippopotam.us/us/' + location.zip.value)
-            .then(function(response) {
-              if (response.ok){
-                return response.json();
-              }
-              throw Error('No data for ZIP code' + location.zip.value);
-            })
-            .then(function(fetchv) {
-              location.city.value = fetchv.places[0]["place name"];
-              location.state.value = fetchv.places[0].state;
-              location.city.classList.add('fade-in');
-              location.state.classList.add('fade-in');
-            })
-            .catch(function(error) {
-              console.log(error);
-              location.city.value = '';
-              location.state.value = '';
-              location.city.classList.add('fade-in');
-              location.state.classList.add('fade-in');
-            });
+      var seatSubmit = document.querySelector('#seat-submit')
+      seatSubmit.addEventListener("click", function(){
+        if(storageAvailable('localStorage')){
+          localStorage.setItem("payment_info2", JSON.stringify(checkout));
+        } // end if statement to check for localStorage
+      });
+    });  // End of DOMContentLoaded
+  }
+  else if (page3 !== null && page3.id == 'check-out' ) {
+    console.log("Checkout page was found");
+    document.addEventListener('DOMContentLoaded', function(){
+      var signup_form = document.querySelector('#payment');
+      var signup_submit=document.querySelector('#submit');// submit button
+
+      if(storageAvailable('localStorage')){
+        var checkout = JSON.parse(localStorage.getItem('payment_info2'));
+        localStorage.removeItem('payment_info2')
+      }
+
+      // listen for keyup event anywhere in the form
+      signup_form.addEventListener('keyup', function(){
+        checkout.email=document.querySelector('#email').value;
+        checkout.phone=document.querySelector('#phonenumber').value;
+        checkout.ccn=document.querySelector('#ccn').value;
+        checkout.cvv=document.querySelector('#cvv').value;
+        checkout.fname= document.querySelector('#fname').value;
+        checkout.lname= document.querySelector('#lname').value;
+
+        // signup_submit.removeAttribute('disabled');
+        signup_submit.setAttribute('disabled', 'disabled');
+
+        // var contact_error = document.querySelector('#contact-error');
+        // Disable signup button if either email or phone number is filled.
+        if (validate_us_phone(checkout.phone) && validate_email(checkout.email) && validate_ccn(checkout.ccn) && validate_cvv(checkout.cvv) && checkout.fname!==null && checkout.lname!==null) {
+          signup_submit.removeAttribute('disabled');
         }
-      });// end of event listener
-    }// end of fetch
+        else {
+          // show user error message
+          if(checkout.phone.length > 10) {
+            console.log("Invalid phone number!");
+          }
+          // This will re-disable the submmit button if the input changes to an invalid state
+          signup_submit.setAttribute('disabled', 'disabled');
+        }
 
-  });  // End of DOMContentLoaded
+        if(storageAvailable('localStorage')){
+          signup_form.addEventListener('submit', function(){
+            localStorage.setItem("payment_info3", JSON.stringify(checkout));
+            console.log("Form submitted and data stored");
+          }); // end blog.form event listener
+        } // end if statement to check for localStorage
+
+      }); // end of key up listener
+
+      var location = {
+        zip: document.querySelector('#zip'),
+        state: document.querySelector('#state'),
+        city: document.querySelector('#city')
+      };
+
+      // Function that calls zippopotam to fill out the form given the zipcode
+      if('fetch' in window) {
+        console.log("yay, this browser suppports the Fetch API");
+        location.city.classList.add('fade-out');
+        location.city.classList.add('fade-out');
+
+        var zip;
+        location.zip.addEventListener('keyup', function() {
+          if (validate_zip(location.zip.value) && zip !== location.zip.value){
+            zip = location.zip.value;
+            fetch('http://api.zippopotam.us/us/' + location.zip.value)
+              .then(function(response) {
+                if (response.ok){
+                  return response.json();
+                }
+                throw Error('No data for ZIP code' + location.zip.value);
+              })
+              .then(function(fetchv) {
+                location.city.value = fetchv.places[0]["place name"];
+                location.state.value = fetchv.places[0].state;
+                location.city.classList.add('fade-in');
+                location.state.classList.add('fade-in');
+              })
+              .catch(function(error) {
+                console.log(error);
+                location.city.value = '';
+                location.state.value = '';
+                location.city.classList.add('fade-in');
+                location.state.classList.add('fade-in');
+              });
+          }
+        });// end of event listener
+      }// end of fetch
+    });  // End of DOMContentLoaded
+  }
+  else if (page4 !== null && page4.id == 'confirmation') {
+    console.log("Confirmation page was found");
+    document.addEventListener('DOMContentLoaded', function(){
+      var checkout = JSON.parse(localStorage.getItem('payment_info3'));
+      console.log(checkout.fname);
+
+      document.getElementById("cdate").innerHTML = 'Time: '+ time();
+      document.getElementById("confirmation-number").innerHTML = 'Confirmation: ' + confirmationTicket();
+      document.getElementById("cfname").innerHTML = 'First Name: ' + checkout.fname;
+      document.getElementById("clname").innerHTML = 'Last Name: ' + checkout.lname;
+      document.getElementById("cemail").innerHTML = 'Email: ' + checkout.email;
+      document.getElementById("cticket-amount").innerHTML = 'Amount of Tickets: ' + checkout.ticketNum;
+      document.getElementById("cticket-price").innerHTML = 'Ticket Price: $' + checkout.ticketTotal;
+      document.getElementById("ctotal").innerHTML = 'Total: $' + checkout.total;
+
+    });  // End of DOMContentLoaded
+  }
+  else {
+    console.log('No page found!');
+  }
+
+
 }());  // End of IIFE
